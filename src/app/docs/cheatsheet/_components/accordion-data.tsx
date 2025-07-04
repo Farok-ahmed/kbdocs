26066f69d395cdfe13c9fb9b71a5c24df23792ea
Farok Ahmed <mdfarokahmed280@gmail.com>
2025-07-04 18:51:28 +0600

    cheatsheet page create

---
+++ src/app/docs/cheatsheet/_components/accordion-data.tsx
@@ -0,0 +1,234 @@
+export default function CheatsheetAccordion() {
+  return (
+    <>
+      <div className="cheatsheet_info">
+        <div
+          className="accordion cheatsheet_accordian"
+          id="accordionExampleatlas"
+        >
+          <div id="auxiliary" className="card">
+            <div className="card-header" id="headingAtlasone">
+              <h2 className="mb-0">
+                <button
+                  className="btn btn-link collapsed"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#collapseAtlasone"
+                  aria-expanded="false"
+                  aria-controls="collapseAtlasone"
+                >
+                  Auxiliary <span className="pluse">[+]</span>
+                  <span className="minus">[-]</span>
+                </button>
+              </h2>
+            </div>
+            <div
+              id="collapseAtlasone"
+              className="collapse"
+              aria-labelledby="headingAtlasone"
+              data-parent="#accordionExampleatlas"
+              style={{}}
+            >
+              <div className="row">
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#1</div>
+                    <p>be</p>
+                    <h4>Sein</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#2</div>
+                    <p>have</p>
+                    <h4>haben</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#3</div>
+                    <p>become</p>
+                    <h4>werden</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#4</div>
+                    <p>can</p>
+                    <h4>konnen</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#4</div>
+                    <p>can</p>
+                    <h4>konnen</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#1</div>
+                    <p>must</p>
+                    <h4>Sein</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#2</div>
+                    <p>may</p>
+                    <h4>haben</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#3</div>
+                    <p>like</p>
+                    <h4>durfen</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="modals" className="card">
+            <div className="card-header" id="headingAtlastwo">
+              <h2 className="mb-0">
+                <button
+                  className="btn btn-link collapsed"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#collapseAtlastwo"
+                  aria-expanded="false"
+                  aria-controls="collapseAtlastwo"
+                >
+                  Modals <span className="pluse">[+]</span>
+                  <span className="minus">[-]</span>
+                </button>
+              </h2>
+            </div>
+            <div
+              id="collapseAtlastwo"
+              className="collapse"
+              aria-labelledby="headingAtlastwo"
+              data-parent="#accordionExampleatlas"
+              style={{}}
+            >
+              <div className="row">
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#1</div>
+                    <p>can</p>
+                    <h4>Sein</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#2</div>
+                    <p>should</p>
+                    <h4>haben</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#3</div>
+                    <p>become</p>
+                    <h4>werden</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#4</div>
+                    <p>want</p>
+                    <h4>konnen</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#3</div>
+                    <p>like</p>
+                    <h4>durfen</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#1</div>
+                    <p>must</p>
+                    <h4>Sein</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#2</div>
+                    <p>may</p>
+                    <h4>haben</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#4</div>
+                    <p>can</p>
+                    <h4>konnen</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="useful" className="card">
+            <div className="card-header" id="headingAtlasthree">
+              <h2 className="mb-0">
+                <button
+                  className="btn btn-link collapsed"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#collapseAtlasthree"
+                  aria-expanded="false"
+                  aria-controls="collapseAtlasthree"
+                >
+                  Useful <span className="pluse">[+]</span>
+                  <span className="minus">[-]</span>
+                </button>
+              </h2>
+            </div>
+            <div
+              id="collapseAtlasthree"
+              className="collapse"
+              aria-labelledby="headingAtlasthree"
+              data-parent="#accordionExampleatlas"
+              style={{}}
+            >
+              <div className="row">
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#1</div>
+                    <p>be</p>
+                    <h4>Sein</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#2</div>
+                    <p>have</p>
+                    <h4>haben</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#3</div>
+                    <p>become</p>
+                    <h4>werden</h4>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  <div className="cheatsheet_item">
+                    <div className="cheatsheet_num">#4</div>
+                    <p>can</p>
+                    <h4>konnen</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/app/docs/cheatsheet/page.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import CheatsheetAccordion from "./_components/accordion-data";
+
+const CheatsheetPage = () => {
+  return (
+    <>
+      <div id="post" className="shortcode_info rvfs-4" data-rvfs={4}>
+        <div className="shortcode_title">
+          <h1>Cheatsheet</h1>
+          <p>
+            <span>Welcome to KbDoc !</span> Get familiar with the Stripe
+            products and explore their features, What bind does is it takes a
+            snapshot of the values of variables that you are passing to
+            functions,
+          </p>
+        </div>
+        <CheatsheetAccordion />
+        <footer>
+          <div className="border_bottom" />
+          <div className="row feedback_link">
+            <div className="col-lg-6">
+              <h6>
+                <i className="icon_mail_alt" />
+                Still stuck?
+                <Link href="#" data-toggle="modal" data-target="#exampleModal3">
+                  How can we help?
+                </Link>
+              </h6>
+            </div>
+            <div className="col-lg-6">
+              <p>
+                Was this page helpful?
+                <Link href="#" className="h_btn">
+                  Yes
+                </Link>
+                <Link href="#" className="h_btn">
+                  No
+                </Link>
+              </p>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default CheatsheetPage;
