51685f1be4d4cb2f14c9c64de1795e3d0ac4c3ed
Farok Ahmed <mdfarokahmed280@gmail.com>
2025-07-08 13:09:42 +0600

    make dynamic slider

--- src/app/cool-knowledge-base/_components/reviews.tsx
+++ src/app/cool-knowledge-base/_components/reviews.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import { useState } from "react";
+
+interface Testimonial {
+  id: string;
+  name: string;
+  photo: string;
+  job: string;
+  review: string;
+}
+
 const Reviews = () => {
-  const reviews = [
+  const [activeIndex, setActiveIndex] = useState(0);
+  const testimonials: Testimonial[] = [
     {
       id: "0",
       name: "Rodney Artichoke",
@@ -14,7 +27,7 @@ const Reviews = () => {
     },
     {
       id: "1",
-      name: "Rodney Artichoke",
+      name: "Farok",
       photo: "img/home_one/icon/feedback.jpg",
       job: "UI/UX designer",
       review: `Nulla porttitor accumsan tincidunt. Praesent
@@ -24,7 +37,34 @@ const Reviews = () => {
             eget felis porttitor volutpat. Proin eget
             tortor risus.`,
     },
-  ].slice(0, 1);
+    {
+      id: "2",
+      name: "Rodney ",
+      photo: "img/home_one/icon/feedback.jpg",
+      job: "UI/UX designer",
+      review: `Nulla porttitor accumsan tincidunt. Praesent
+            sapien massa, convallis a pellentesque nec,
+            egestas non nisi. Nulla quis lorem ut libero
+            malesuada feugiat. Vivamus suscipit tortor
+            eget felis porttitor volutpat. Proin eget
+            tortor risus.`,
+    },
+  ];
+  // 3. Handlers to navigate between testimonials
+  const handlePrev = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === 0 ? testimonials.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNext = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === testimonials.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const activeTestimonial = testimonials[activeIndex];
+
   return (
     <>
       <section
@@ -38,24 +78,20 @@ const Reviews = () => {
         <div className="container">
           <div className="doc_feedback_info">
             <div className="doc_feedback_slider">
-              {reviews.map(({ id, name, photo, job, review }) => {
-                return (
-                  <div key={id} className="item">
-                    <div className="author_img">
-                      <img src={photo} alt="" />
-                    </div>
-                    <p>{review}</p>
-                    <h5>{name}</h5>
-                    <h6>{job}</h6>
-                  </div>
-                );
-              })}
+              <div className="item">
+                <div className="author_img">
+                  <img src={activeTestimonial.photo} alt="" />
+                </div>
+                <p>{activeTestimonial.review}</p>
+                <h5>{activeTestimonial.name}</h5>
+                <h6>{activeTestimonial.job}</h6>
+              </div>
             </div>
             <div className="slider_nav">
-              <div className="prev">
+              <div className="prev" onClick={handlePrev}>
                 <span className="arrow"></span>
               </div>
-              <div className="next">
+              <div className="next" onClick={handleNext}>
                 <span className="arrow"></span>
               </div>
             </div>
